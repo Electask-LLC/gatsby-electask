c1189fe61a4e58b8b13ab397ed3f4f2f96ed90c5
thall1961 <thall1961@gmail.com>
2019-06-18 11:45:17 -0600

    take out google analytics - already in tag manager

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,30 +11,6 @@ module.exports = {
         description: `Electask helps you manage your election tasks.  With the help of Electask, you'll have smoother elections and better communication.  Stay on top of election tasks.`
     },
     plugins: [
-        {
-            resolve: `gatsby-plugin-google-analytics`,
-            options: {
-                trackingId: 'UA-106586103-2',
-                // Defines where to place the tracking script - `true` in the head and `false` in the body
-                head: true,
-                // Setting this parameter is optional
-                anonymize: true,
-                // Setting this parameter is also optional
-                respectDNT: true,
-                // Avoids sending pageview hits from custom paths
-                // exclude: ['/preview/**', '/do-not-track/me/too/'],
-                // Enables Google Optimize using your container Id
-                // optimizeId: 'YOUR_GOOGLE_OPTIMIZE_TRACKING_ID',
-                // // Enables Google Optimize Experiment ID
-                // experimentId: 'YOUR_GOOGLE_EXPERIMENT_ID',
-                // // Set Variation ID. 0 for original 1,2,3....
-                // variationId: 'YOUR_GOOGLE_OPTIMIZE_VARIATION_ID',
-                // // Any additional create only fields (optional)
-                sampleRate: 5,
-                siteSpeedSampleRate: 10,
-                cookieDomain: 'electask.com'
-            }
-        },
         `gatsby-plugin-sitemap`,
         `gatsby-plugin-react-helmet`,
         {
