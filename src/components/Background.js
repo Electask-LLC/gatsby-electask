e5196bdf333780afb13d9b3733f0da8a3a2ba38b
moltersd <89766417+moltersd@users.noreply.github.com>
2021-10-11 20:06:35 -0400

    Update Background.js

--- src/components/Background.js
+++ src/components/Background.js
@@ -7,32 +7,20 @@ function Background({bg}) {
             <div className="col-md-6 col-lg-4 mx-auto">
                 <h2 className="text-muted">Background</h2>
                 <p>
-                    I worked for Voter Sacramento County Registration &amp;
-                    Elections before, during, and after the 2014 Gubernatorial
-                    Election so I got to see first hand how they prepared for,
-                    managed, and completed an election. It was great experience
-                    and let me see how many improvements there were to be made
-                    in the tech and process.
+                    Electask grew out of a custom project for Sacramento County, who were 
+                    looking to replace their Excel-based election task management 
+                    with software. When they couldn't find a solution, we built Electask!
                 </p>
                 <p>
-                    A few years after my time with the County, the Executive
-                    Secretary at the time, knowing my programming experience,
-                    reached out to me for advice on how to improve their task
-                    management process. I built the first version of Electask
-                    and had them try it out.
+                    Electask allow counties to track their tasks in one place without an expensive
+                    election management suite. This allows counties to be better stewards of taxpayer
+                    dollars.
                 </p>
                 <p>
-                    After a few tweaks, they were up and running with it and
-                    implemented it into their core task management process. Now,
-                    several improvements later (and more to come), Sacramento,
-                    along with other counties, have found value in what Electask
-                    can do.
-                </p>
-                <p>
-                    The bottom line is it makes their life easier. It helps
-                    everyone stay up to date and on the same page. And many more
-                    exciting features and improvements are scheduled for the
-                    future.
+                    We wanted to help because we've worked in local and state government and
+                    want to make sure that they get top-notch software at a great price. We'd
+                    love to talk to you about your county's needs!
+                
                 </p>
             </div>
         </div>
